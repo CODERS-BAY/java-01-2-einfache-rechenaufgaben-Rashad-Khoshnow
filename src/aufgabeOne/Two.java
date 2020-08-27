b77a03c0fb502f42c51bf752b8ee3138c6fb7425
Rashad <rashad.khoshnow@gmail.com>
2020-08-27 11:35:19 +0200

    Add double and multiplied

--- src/aufgabeOne/Two.java
+++ src/aufgabeOne/Two.java
@@ -3,12 +3,21 @@ package aufgabeOne;
 public class Two {
     public static void main(String[] args) {
 
-        int nummer1 = 3;
-        int nummer2 = 25;
+        int number1 = 3;
+        int number2 = 25;
+        double number3 = 55.4;
+        double number4 = 23.5;
 
-        System.out.println(nummer1 + nummer2);
-        System.out.println(nummer1 - nummer2);
-        System.out.println(nummer1 / nummer2);
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 * number2);
+
+        System.out.println(number3 + number4);
+        System.out.println(number3 - number4);
+        System.out.println(number3 / number4);
+        System.out.println(number3 * number4);
     }
 
 }
