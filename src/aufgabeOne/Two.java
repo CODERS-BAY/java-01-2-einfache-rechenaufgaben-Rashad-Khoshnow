8972ce4bbbbd74da6bb1cad2fda3792454e0bd7f
Rashad <rashad.khoshnow@gmail.com>
2020-08-27 09:35:46 +0200

    Add main class

---
+++ src/aufgabeOne/Two.java
@@ -0,0 +1,14 @@
+package aufgabeOne;
+
+public class Two {
+    public static void main(String[] args) {
+
+        int nummer1 = 3;
+        int nummer2 = 25;
+
+        System.out.println(nummer1 + nummer2);
+        System.out.println(nummer1 - nummer2);
+        System.out.println(nummer1 / nummer2);
+    }
+
+}
